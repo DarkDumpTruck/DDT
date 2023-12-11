1b29b23bb5ca032a3cb269ae73b2d8e759e8959c
DarkDumpTruck <131649948+DarkDumpTruck@users.noreply.github.com>
2023-12-11 22:19:13 +0800

    Fix CI

--- scripts/run-tests-with-multiple-quoridor-board-size.sh
+++ scripts/run-tests-with-multiple-quoridor-board-size.sh
@@ -3,14 +3,12 @@
 set -e
 
 cd cpp
-rm -rf ./build
 
 for w in $(seq 6 11);
 do
 for h in $(seq 6 11);
 do
-meson setup -Dcpp_args="-ffast-math -march=native -DQUORIDOR_WIDTH=$w -DQUORIDOR_HEIGHT=$h" build --buildtype=release
-meson compile -C build
-meson test -C build --print-errorlogs
+meson setup -Dcpp_args="-DQUORIDOR_WIDTH=$w -DQUORIDOR_HEIGHT=$h" build --buildtype=release --reconfigure
+meson test -C build --print-errorlogs game_quoridor
 done
 done
